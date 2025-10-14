a32aff22a739df438e9656ffb0ac4cc5e97f54f1
zn <16239955+zn_1109@user.noreply.gitee.com>
2025-10-14 17:04:27 +0800

    更新了合约文件

--- 合约/shipment.sol
+++ 合约/shipment.sol
@@ -6,21 +6,47 @@ contract Shipment{
     struct user{//货主
         uint id;
         string name;
-
+        string passwd;
     }
     struct carrier{//承运商
         uint id;
         string name;
         string info;
+        string passwd;
     }
     struct shipment{//物流信息
         uint id;
         string from;
         string to;
-        string statous;
+        string status;
         uint createTime;
-        uint updataTime;
-        
+        uint updateTime;
+    }
+    user[] users;
+    carrier[] carriers;
+
+    function addUser(string _name,string _passwd) public{
+        user memory u = user(users.length+1,_name,_passwd);//创建一个新的货主对象
+        users.push(u);//将货主对象存储至数组中
+    }
+
+    function checkUser(uint _id,string _passwd)public view returns(bool){
+        if(keccak256(users[_id-1].passwd)==keccak256(_passwd)){
+            return true;
+        }
+        return false;
+    }
+
+
+    function addCarrier(string _name,string _info,string _passwd) public{
+        carrier memory c = carrier(carriers.length+1,_name,_info,_passwd);//创建一个新的承运商对象
+        carriers.push(c);//将承运商对象存储至数组中
     }
     
+    function checkCarrier(uint _id,string _passwd)public view returns(bool){
+        if(keccak256(carriers[_id-1].passwd)==keccak256(_passwd)){
+            return true;
+        }
+        return false;
+    }
 }
